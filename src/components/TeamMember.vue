54257f74107c0d527199167359dce4e809f890a1
Jan Gawarecki <jan.gawarecki@students.coding-bootcamps.eu>
2021-06-22 10:01:48 +0200

    TeamMember component added

---
+++ src/components/TeamMember.vue
@@ -0,0 +1,25 @@
+<template></template
+
+<script>
+    emport default {
+        name: "TeamMember",
+        props: {
+            foto: {
+                type: Image
+
+            }
+            bullet1: {
+                type: String
+
+            }
+            bullet2: {
+                type: String
+            }
+            bullet3: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style></style>
