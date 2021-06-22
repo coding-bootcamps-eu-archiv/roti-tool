bae6b8b00f2887e027eae6c71cd60dd97aaedb77
Nicolai Simon <nicolai.simon@students.coding-bootcamps.eu>
2021-06-22 11:17:05 +0200

    Index Page: created/imported instruction component

---
+++ src/components/InstructionCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>{{ headline }}</h3>
+    <img :src="picture_src" />
+    <div class="pic_sub">{{ pic_desc }}</div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "InstructionCard",
+  props: {
+    headline: String,
+    description: String,
+    picture_src: String,
+    pic_desc: String,
+  },
+};
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -12,11 +12,34 @@
     <router-link to="/NewRoti" tag="button">Klick fürs ROTI</router-link>
   </button>
   <br />
+  <article>
+    <h2>Anleitung</h2>
+    <div>
+      <InstructionCard
+        headline="Schritt 1"
+        description="Das hold Scharmützel anschwärzen. Die dufte Freikörperkultur jauchzen. Mein lieber Scholli!"
+        picture_src="https://images.unsplash.com/photo-1534481016308-0fca71578ae5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW5zdHJ1Y3Rpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      />
+      <InstructionCard
+        headline="Schritt 2"
+        description="Die kess Lunte auftakeln. Freikörperkultur und Herrengedeck verhaspeln einfältig Fuchtel."
+        picture_src="https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aW5zdHJ1Y3Rpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      />
+      <InstructionCard
+        headline="Schritt 3"
+        description="Das töricht Schenkelbürste betören. Das Übeltäter frickeln das grobschlächtig Schluckspecht. Der butterweich Schabracke. Die kess Lunte auftakeln. Freikörperkultur und Herrengedeck verhaspeln einfältig Fuchtel. Das kess Tölpel berappen. Flausen und Promenadenmischung verhaspeln famos Fräulein."
+        picture_src="https://images.unsplash.com/photo-1589803299004-b9143b44275f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGluc3RydWN0aW9ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      />
+    </div>
+  </article>
+  <br />
 </template>
 
 <script>
+import InstructionCard from "@/components/InstructionCard.vue";
+
 export default {
   name: "Index",
-  components: {},
+  components: { InstructionCard },
 };
 </script>
