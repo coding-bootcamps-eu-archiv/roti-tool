7a66777557ae6f4552630646eb31aca9bff5177e
Jan Gawarecki <jan.gawarecki@students.coding-bootcamps.eu>
2021-06-23 10:00:52 +0200

    TeamMember.js added

---
+++ src/components/TeamMember.js
@@ -0,0 +1,38 @@
+const members = [
+  {
+    foto_url: "/static/zebra.jpg",
+    name: "Otto",
+    bullet1: "abc",
+    bullet2: "abc",
+    bullet3: "abc",
+  },
+  {
+    foto: "Design Patterns",
+    name: "Paula",
+    bullet1: "abc",
+    bullet2: "abc",
+    bullet3: "abc",
+  },
+  {
+    foto: "Design Patterns",
+    name: "Siegfried",
+    bullet1: "abc",
+    bullet2: "abc",
+    bullet3: "abc",
+  },
+  {
+    foto: "Design Patterns",
+    name: "Ingrid",
+    bullet1: "abc",
+    bullet2: "abc",
+    bullet3: "abc",
+  },
+  {
+    foto: "Design Patterns",
+    name: "Roy",
+    bullet1: "abc",
+    bullet2: "abc",
+    bullet3: "abc",
+  },
+];
+export default members;
