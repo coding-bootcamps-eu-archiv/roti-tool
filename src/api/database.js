97d5d7eb2d79145604247a172ddc6692c80b2768
Torsten Bisanz-Blank <torsten.bisanz-blank@students.coding-bootcamps.eu>
2021-06-24 11:44:31 +0200

    connected with firebase

--- src/api/database.js
+++ src/api/database.js
@@ -1,4 +1,27 @@
+/**
+     * 
+thema
+trainer
+teachingAssistent
+date
+rotiValue
+rotiText
+*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5SUwgvvVciUho7HwpbjJTMtWjCywPUfA",
+  authDomain: "roti-tool-e1dac.firebaseapp.com",
+  projectId: "roti-tool-e1dac",
+  storageBucket: "roti-tool-e1dac.appspot.com",
+  messagingSenderId: "544674597566",
+  appId: "1:544674597566:web:01d19652c123919ad8acef",
+  measurementId: "G-QYGG8RG541",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 export default {
-  createRoti: () => {},
+  createRoti: (newRoti) => {},
   getRotis: () => {},
 };
