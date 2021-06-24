396722fe0b42337a77286354637a8d3526903e6b
vanessa <vanessa.wagner@students.coding-bootcamps.eu>
2021-06-24 11:05:10 +0200

    Eingabemaske: changed names of select-tags

--- src/views/NewRoti.vue
+++ src/views/NewRoti.vue
@@ -2,24 +2,28 @@
   <h2>Neuer ROTI Eintrag</h2>
 
   <form class="input-roti-form">
-    <select name="Thema" id="thema" class="dropdown-items">
-      <option value="volvo">--Thema--</option>
+    <select name="thema" id="thema" class="dropdown-items" required>
+      <option value=" ">--Thema--</option>
       <option value="volvo">Volvo</option>
       <option value="saab">Saab</option>
       <option value="mercedes">Mercedes</option>
       <option value="audi">Audi</option>
     </select>
 
-    <select name="Trainer" id="trainer" class="dropdown-items">
-      <option value="volvo">--Trainer--</option>
+    <select name="trainer" id="trainer" class="dropdown-items">
+      <option value=" ">--Trainer--</option>
       <option value="volvo">Volvo</option>
       <option value="saab">Saab</option>
       <option value="mercedes">Mercedes</option>
       <option value="audi">Audi</option>
     </select>
 
-    <select name="TA" id="ta" class="dropdown-items">
-      <option value="volvo">--Teaching Assistent--</option>
+    <select
+      name="teachingAssistent"
+      id="teachingAssistent"
+      class="dropdown-items"
+    >
+      <option value=" ">--Teaching Assistent--</option>
       <option value="volvo">Volvo</option>
       <option value="saab">Saab</option>
       <option value="mercedes">Mercedes</option>
@@ -27,7 +31,7 @@
     </select>
 
     <input
-      placeholder="Date"
+      placeholder="date"
       class="textbox-n dropdown-items"
       type="date"
       id="date"
@@ -35,29 +39,29 @@
     <div class="full-width">
       <p>Gib deine Ranking ab</p>
       <span class="radio-btn">
-        <input type="radio" name="filter" id="1" value="1" />
+        <input type="radio" name="rotiValue" id="1" value="1" />
         <label for="1">1</label>
       </span>
       <span class="radio-btn">
-        <input type="radio" name="filter" id="2" value="2" />
+        <input type="radio" name="rotiValue" id="2" value="2" />
         <label for="2">2</label>
       </span>
       <span class="radio-btn">
-        <input type="radio" name="filter" id="3" value="3" />
+        <input type="radio" name="rotiValue" id="3" value="3" />
         <label for="3">3</label>
       </span>
       <span class="radio-btn">
-        <input type="radio" name="filter" id="4" value="4" />
+        <input type="radio" name="rotiValue" id="4" value="4" />
         <label for="4">4</label>
       </span>
       <span class="radio-btn">
-        <input type="radio" name="filter" id="5" value="5" />
+        <input type="radio" name="rotiValue" id="5" value="5" />
         <label for="5">5</label>
       </span>
     </div>
     <textarea
       class="full-width"
-      name="Comment"
+      name="comment"
       id="comment"
       cols="30"
       rows="10"
@@ -79,12 +83,6 @@
   justify-self: center;
   width: 30rem;
   height: 2rem;
-}
-
-.thema-trainer {
-}
-
-.thema {
 }
 
 .full-width {
@@ -115,5 +113,13 @@ button {
   font-size: 1rem;
   font-weight: bold;
   border-radius: 15px;
+}
+
+select:invalid {
+  border: 2px dashed red;
+}
+
+select:valid {
+  border: 2px solid black;
 }
 </style>
