f7b56bfe815b32fc4662345af72aa69e737418ca
Torsten Bisanz-Blank <torsten.bisanz-blank@students.coding-bootcamps.eu>
2021-06-28 08:58:16 +0200

    prepared for api

--- src/api/database.js
+++ src/api/database.js
@@ -7,30 +7,6 @@ date
 rotiValue
 rotiText
 */
-import firebase from "firebase/app";
-
-import * as admin from "firebase-admin";
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    "https://roti-tool-e1dac-default-rtdb.europe-west1.firebasedatabase.app",
-});
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB5SUwgvvVciUho7HwpbjJTMtWjCywPUfA",
-  authDomain: "roti-tool-e1dac.firebaseapp.com",
-  projectId: "roti-tool-e1dac",
-  storageBucket: "roti-tool-e1dac.appspot.com",
-  messagingSenderId: "544674597566",
-  appId: "1:544674597566:web:01d19652c123919ad8acef",
-  measurementId: "G-QYGG8RG541",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 export default {
   createRoti: (
@@ -40,18 +16,12 @@ export default {
     date,
     rotiValue,
     rotiText
-  ) => {
-    firebase
-      .database()
-      .ref("users/" + userId)
-      .set({
-        thema: thema,
-        trainer: trainer,
-        teachingAssistent: teachingAssistent,
-        date: date,
-        rotiValue: rotiValue,
-        rotiText: rotiText,
+  ) => {},
+  getRotis: () => {
+    fetch("http://localhost:4730/books")
+      .then((response) => response.json())
+      .then((rotis) => {
+        this.rotis = rotis;
       });
   },
-  getRotis: () => {},
 };
