febfd1a125013cefc408a9cecbb818b85d9a711c
vanessa <vanessa.wagner@students.coding-bootcamps.eu>
2021-06-28 14:36:58 +0200

    Eingabemaske: Validation

--- src/views/NewRoti.vue
+++ src/views/NewRoti.vue
@@ -2,52 +2,58 @@
   <h2>Neuer ROTI Eintrag</h2>
 
   <Form class="input-roti-form" @submit="onSubmit">
-    <Field
-      as="select"
-      name="topic"
-      id="topics"
-      class="dropdown-items"
-      :rules="isRequired"
-    >
-      <option value="">--Thema--</option>
-      <option v-for="topics in topics" :key="topics.id" :value="topic">
-        {{ topics.description }}
-      </option>
-    </Field>
-    <error-message name="topic"></error-message>
-
-    <Field
-      as="select"
-      name="trainer"
-      id="trainer"
-      class="dropdown-items"
-      :rules="isRequired"
-    >
-      <option value=" ">--Trainer--</option>
-      <option :value="trainer" v-for="trainer in trainer" :key="trainer.id">
-        {{ trainer.name }}
-      </option>
-    </Field>
-    <error-message name="trainer"></error-message>
-
-    <Field
-      name="teachingAssistent"
-      id="teachingAssistent"
-      class="dropdown-items"
-      as="select"
-      :rules="isRequired"
-    >
-      <option value="">--Teaching Assistent--</option>
-      <option
-        :value="teachingAssistent"
-        v-for="teachingAssistent in teachingAssistent"
-        :key="teachingAssistent.id"
+    <div>
+      <Field
+        as="select"
+        name="topic"
+        id="topics"
+        class="dropdown-items"
+        :rules="isRequired"
       >
-        {{ teachingAssistent.name }}
-      </option>
-    </Field>
-    <error-message name="teachingAssistent"></error-message>
-
+        <option value="">--Thema--</option>
+        <option v-for="topics in topics" :key="topics.id" :value="topic">
+          {{ topics.description }}
+        </option>
+      </Field>
+      <br />
+      <error-message name="topic"></error-message>
+    </div>
+    <div>
+      <Field
+        as="select"
+        name="trainer"
+        id="trainer"
+        class="dropdown-items"
+        :rules="isRequired"
+      >
+        <option value="">--Trainer--</option>
+        <option :value="trainer" v-for="trainer in trainer" :key="trainer.id">
+          {{ trainer.name }}
+        </option>
+      </Field>
+      <br />
+      <error-message name="trainer"></error-message>
+    </div>
+    <div>
+      <Field
+        name="teachingAssistent"
+        id="teachingAssistent"
+        class="dropdown-items"
+        as="select"
+        :rules="isRequired"
+      >
+        <option value="">--Teaching Assistent--</option>
+        <option
+          :value="teachingAssistent"
+          v-for="teachingAssistent in teachingAssistent"
+          :key="teachingAssistent.id"
+        >
+          {{ teachingAssistent.name }}
+        </option>
+      </Field>
+      <br />
+      <error-message name="teachingAssistent"></error-message>
+    </div>
     <input
       placeholder="date"
       class="textbox-n dropdown-items"
@@ -55,37 +61,60 @@
       id="date"
     />
     <div class="full-width">
-      <p>Gib deine Ranking ab</p>
-      <span class="radio-btn">
-        <input type="radio" name="rotiValue" id="1" value="1" />
-        <label for="1">1</label>
-      </span>
-      <span class="radio-btn">
-        <input type="radio" name="rotiValue" id="2" value="2" />
-        <label for="2">2</label>
-      </span>
-      <span class="radio-btn">
-        <input type="radio" name="rotiValue" id="3" value="3" />
-        <label for="3">3</label>
-      </span>
-      <span class="radio-btn">
-        <input type="radio" name="rotiValue" id="4" value="4" />
-        <label for="4">4</label>
-      </span>
-      <span class="radio-btn">
-        <input type="radio" name="rotiValue" id="5" value="5" />
-        <label for="5">5</label>
-      </span>
+      <Field
+        as="radio"
+        name="ranking"
+        id="ranking"
+        class="radio-btn"
+        :rules="isRequired"
+      >
+        <p>Gib deine Ranking ab</p>
+        <span class="radio-btn">
+          <input type="radio" name="rotiValue" id="1" value="1" />
+          <label for="1">1</label>
+        </span>
+        <span class="radio-btn">
+          <input type="radio" name="rotiValue" id="2" value="2" />
+          <label for="2">2</label>
+        </span>
+        <span class="radio-btn">
+          <input type="radio" name="rotiValue" id="3" value="3" />
+          <label for="3">3</label>
+        </span>
+        <span class="radio-btn">
+          <input type="radio" name="rotiValue" id="4" value="4" />
+          <label for="4">4</label>
+        </span>
+        <span class="radio-btn">
+          <input type="radio" name="rotiValue" id="5" value="5" />
+          <label for="5">5</label>
+        </span>
+      </Field>
+      <br />
+      <error-message name="ranking"></error-message>
     </div>
-    <textarea
-      class="full-width"
-      name="comment"
-      id="comment"
-      cols="30"
-      rows="10"
-      placeholder="Gib dein Kommentar ab (1000 Zeichen)"
-      maxlength="1000"
-    ></textarea>
+    <div>
+      <Field
+        as="textarea"
+        name="textarea"
+        id="textarea"
+        class="textarea"
+        :rules="isRequired"
+      >
+        <textarea
+          class="full-width"
+          name="comment"
+          id="comment"
+          cols="30"
+          rows="10"
+          placeholder="Gib dein Kommentar ab (1000 Zeichen)"
+          maxlength="1000"
+        ></textarea>
+      </Field>
+
+      <br />
+      <error-message name="textarea"></error-message>
+    </div>
     <button class="full-width">Absenden</button>
   </Form>
 </template>
@@ -122,6 +151,15 @@ export default {
 </script>
 
 <style>
+label {
+  font-size: 2.3rem;
+}
+
+#textarea {
+  width: 800px;
+  height: 100px;
+}
+
 .input-roti-form {
   display: grid;
   grid-template-columns: 1fr 1fr;
