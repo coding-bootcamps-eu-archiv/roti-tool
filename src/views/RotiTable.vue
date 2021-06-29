8355f834cef5a50186699089b420601db198c1a5
Torsten Bisanz-Blank <torsten.bisanz-blank@students.coding-bootcamps.eu>
2021-06-29 20:48:58 +0200

    display rotis in percent

--- src/views/RotiTable.vue
+++ src/views/RotiTable.vue
@@ -5,12 +5,20 @@
   <div>
     Filter nach Trainer:
     <input type="text" placeholder="Trainer" v-model="inputValueTrainer" />
-    Ergebnisse: {{ filteredRotis.length }}
   </div>
   <div>
     Filter nach Thema:
     <input type="text" placeholder="Thema" v-model="inputValueThema" />
-    Ergebnisse: {{ filteredRotis.length }}
+  </div>
+  Ergebnisse: {{ filteredRotis.length }}
+
+  <div>
+    Roti Verteilung in Prozent:
+    <P>1: {{ rotisWith1PointPercent }}%</P>
+    <P>2: {{ rotisWith2PointsPercent }}%</P>
+    <P>3: {{ rotisWith3PointsPercent }}%</P>
+    <P>4: {{ rotisWith4PointsPercent }}%</P>
+    <P>5: {{ rotisWith5PointsPercent }}%</P>
   </div>
   <table class="list">
     <thead>
@@ -54,7 +62,63 @@ export default {
           roti.thema.includes(this.inputValueThema)
       );
     },
+    rotisWith1Point() {
+      return this.rotis.filter(
+        (roti) =>
+          roti.rotiValue.includes("1") &&
+          roti.trainer.includes(this.inputValueTrainer) &&
+          roti.thema.includes(this.inputValueThema)
+      );
+    },
+    rotisWith1PointPercent() {
+      return (100 / this.filteredRotis.length) * this.rotisWith1Point.length;
+    },
+    rotisWith2Points() {
+      return this.rotis.filter(
+        (roti) =>
+          roti.rotiValue.includes("2") &&
+          roti.trainer.includes(this.inputValueTrainer) &&
+          roti.thema.includes(this.inputValueThema)
+      );
+    },
+    rotisWith2PointsPercent() {
+      return (100 / this.filteredRotis.length) * this.rotisWith2Points.length;
+    },
+    rotisWith3Points() {
+      return this.rotis.filter(
+        (roti) =>
+          roti.rotiValue.includes("3") &&
+          roti.trainer.includes(this.inputValueTrainer) &&
+          roti.thema.includes(this.inputValueThema)
+      );
+    },
+    rotisWith3PointsPercent() {
+      return (100 / this.filteredRotis.length) * this.rotisWith3Points.length;
+    },
+    rotisWith4Points() {
+      return this.rotis.filter(
+        (roti) =>
+          roti.rotiValue.includes("4") &&
+          roti.trainer.includes(this.inputValueTrainer) &&
+          roti.thema.includes(this.inputValueThema)
+      );
+    },
+    rotisWith4PointsPercent() {
+      return (100 / this.filteredRotis.length) * this.rotisWith4Points.length;
+    },
+    rotisWith5Points() {
+      return this.rotis.filter(
+        (roti) =>
+          roti.rotiValue.includes("5") &&
+          roti.trainer.includes(this.inputValueTrainer) &&
+          roti.thema.includes(this.inputValueThema)
+      );
+    },
+    rotisWith5PointsPercent() {
+      return (100 / this.filteredRotis.length) * this.rotisWith5Points.length;
+    },
   },
+
   created() {
     dataBase
       .getRotis()
